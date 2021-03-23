b344d51d1541a25b2a3119ff8b2657d5c84008ef
galp76 <germanlobo76@gmail.com>
2021-03-23 12:05:30 -0400

    Primer commit de este repositorio

---
+++ ahorcado.py
@@ -0,0 +1,122 @@
+import time
+
+
+def formato(n):
+    if n == 0:
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+
+
+    if n == 1:
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' ')
+        print(' _______')
+        print('')
+
+
+    if n == 2:
+        print('')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 3:
+        print('_______')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 4:
+        print('_______')
+        print('|     |')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 5:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 6:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|     |')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 7:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|    /|')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 8:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|    /|\\')
+        print('|')
+        print('|')
+        print('|_______')
+        print('')
+
+
+    if n == 9:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|    /|\\')
+        print('|    / ')
+        print('|')
+        print('|_______')
+        print('')
+
+    if n == 10:
+        print('_______')
+        print('|     |')
+        print('|     0')
+        print('|    /|\\')
+        print('|    / \\')
+        print('|')
+        print('|_______')
+        print('')
+
